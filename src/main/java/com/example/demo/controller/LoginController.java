b45202f6ab0e0ff795b53e011c66ed326b54fccb
jfdev <jfdev73@gmail.com>
2024-05-19 22:35:50 -0600

    validando el bloqueo de usuario en el controller

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,5 +1,9 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.LoginAttemptService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,8 +15,18 @@ import java.security.Principal;
 @Controller
 public class LoginController {
 
+    @Autowired
+    private LoginAttemptService loginAttemptService;
+    //logger
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+
     @GetMapping("/login")
+
+
     public String login(@RequestParam(required = false) String error, Model model, Principal principal, RedirectAttributes flash) {
+        if(loginAttemptService.isBlocked()) {
+            logger.error("User is blocked");
+        }
         if (principal != null) {
             flash.addFlashAttribute("info", "Ya ha iniciado sesión anteriormente");
             return "redirect:/usuario/list";
